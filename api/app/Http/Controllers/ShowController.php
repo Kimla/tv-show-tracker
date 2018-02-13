c607ba3ee77b414171a081fb38cf9def945a1b3b
Kim Larsson <kimla89@gmail.com>
2018-02-13 22:00:39 +0100

    Removed some unused code

--- api/app/Http/Controllers/ShowController.php
+++ api/app/Http/Controllers/ShowController.php
@@ -2,22 +2,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\User;
 use App\UserShow;
 use App\Show;
-use Tymon\JWTAuth\JWTAuth;
 use DB;
 
 class ShowController extends Controller
 {
-
-    protected $jwt;
-
-    public function __construct(JWTAuth $jwt)
-    {
-        $this->jwt = $jwt;
-    }
-
     public function popularShows() {
         $results = UserShow::groupBy('show_id')
             ->orderBy('count', 'desc')
